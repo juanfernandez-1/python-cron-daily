efa47e8bb3ed65b37e25790945f1011a49fc6cb8
juanfernandez-1 <juanfernandezsolaria@gmail.com>
2025-11-14 13:17:47 +0100

    prueba vista movil

--- radar_mercados_global_html.py
+++ radar_mercados_global_html.py
@@ -248,6 +248,7 @@ html_page = f"""<!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Informe mercados – {fecha_str}</title>
     <style>
         body {{
